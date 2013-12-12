57d734fbfd95aea01d2b97e289ed4c50628ae0c8
Marvin Steadfast <xsteadfastx@gmail.com>
2013-12-12 11:08:24 +0100

    more beautiful variables

--- LittleFeedPoster.py
+++ LittleFeedPoster.py
@@ -3,48 +3,48 @@ from time import sleep
 
 
 class LittleFeedPoster(object):
-    def __init__(self, FeedName, FeedURL):
-        self.FeedURL = FeedURL
-        self.FeedName = FeedName
-        self.OldFeed = feedparser.parse(self.FeedURL)
-        self.OldEntry = self.OldFeed['entries'][0]['link']
+    def __init__(self, feed_name, feed_url):
+        self.feed_url = feed_url
+        self.feed_name = feed_name
+        self.old_feed = feedparser.parse(self.feed_url)
+        self.old_entry = self.old_feed['entries'][0]['link']
 
-    def NewItem(self):
-        self.NewFeed = feedparser.parse(self.FeedURL)
-        NewEntry = self.NewFeed['entries'][0]['link']
-        if NewEntry != self.OldEntry:
-            self.OldEntry = NewEntry
+    def has_new_item(self):
+        self.new_feed = feedparser.parse(self.feed_url)
+        new_entry = self.new_feed['entries'][0]['link']
+        if new_entry != self.old_entry:
+            self.old_entry = new_entry
             return True
         else:
             return False
 
-    def SendNewItem(self):
-        if self.NewItem():
-            message = self.NewFeed['entries'][0]['title'] + ' ' + self.NewFeed['entries'][0]['link']
+    def send_new_item(self):
+        if self.has_new_item():
+            message = self.new_feed['entries'][0]['title'] + ' ' + self.new_feed['entries'][0]['link']
             print message
         else:
             pass
 
-def ReadFeedFile(FileName):
+def read_feed_file(file_name):
     feeds = {}
-    with open(FileName, 'r') as FeedList:
-        for line in FeedList:
+    with open(file_name, 'r') as feed_list:
+        for line in feed_list:
             (key, value) = line.split(' ')
             feeds[key] = value
     return feeds
 
-def GenerateFeeds(FeedList):
-    MyFeeds = []
-    for key, value in FeedList.items():
-        NewFeed = LittleFeedPoster(key, value)
-        MyFeeds.append(NewFeed)
-    return MyFeeds
+def generate_feeds(feed_list):
+    my_feeds = []
+    for key, value in feed_list.items():
+        new_feed = LittleFeedPoster(key, value)
+        my_feeds.append(new_feed)
+    return my_feeds
 
 if __name__ == "__main__":
-    MyFeedList = ReadFeedFile('feeds.list')
-    MyFeeds = GenerateFeeds(MyFeedList)
+    my_feed_list = read_feed_file('feeds.list')
+    my_feeds = generate_feeds(my_feed_list)
 
     while True:
-        for feed in MyFeeds:
-            feed.SendNewItem()
+        for feed in my_feeds:
+            feed.send_new_item()
         sleep(60)
